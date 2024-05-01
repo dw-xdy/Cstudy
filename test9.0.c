7a350aae511416be160b065339aa0c6c7fe664c5
dw-xdy <3115708251@qq.com>
2024-05-02 01:12:22 +0800

    C语言学习
    开始了,新的1000行

---
+++ test9.0.c
@@ -0,0 +1,10 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int a[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+//	int (*p)[5];//这个的意义是：将“p”指向一个长度为“5”的数组!
+//	return 0;
+//}
+
